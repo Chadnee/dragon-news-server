39cd917f4f5c1e241532f6380890703f4bb710e4
Mstchadnee <116938357+Mstchadnee@users.noreply.github.com>
2023-07-28 01:06:59 +0600

    additional verification

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
     
     const dragonCollection = client.db("dragon-db").collection("dragon-collection")
     
